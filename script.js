e11109c2eebb74c8757aec91f0ff809ba4db005c
bbang2 <power3869@gmail.com>
2023-01-19 13:01:22 +0900

    add audio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function buttonActive(e) {
     const keyButton = document.querySelector(`.key[data-number='${e.keyCode}']`);
+    if (!keyButton) {return;}
     keyButton.classList.add("playing");
 }
 
@@ -7,6 +8,14 @@ function removeTransition(e) {
     this.classList.remove("playing");
 }
 
+function playAudio(e) {
+    const audio = document.querySelector(`audio[data-number="${e.keyCode}"]`);
+    if (!audio) {return;}
+    audio.currentTime = 0;
+    audio.play();
+}
+
 window.addEventListener("keydown", buttonActive);
+window.addEventListener("keydown", playAudio);
 const keys = document.querySelectorAll(".keys .key");
 keys.forEach(key => {key.addEventListener("transitionend", removeTransition);})
