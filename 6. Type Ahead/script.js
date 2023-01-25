ddd522ebf0e2923eb4ebc94ad0343f4cc7e135bd
bbang2 <power3869@gmail.com>
2023-01-25 22:52:19 +0900

    6.2. Type Ahead fetch cities data

---
+++ 6. Type Ahead/script.js
@@ -0,0 +1,15 @@
+// 1. fetch the city data
+const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+
+fetch(endpoint)
+    .then(response => response.json())
+    // .then(response => response.text())
+    // .then(result => JSON.parse(result))
+    .then(data => cities.push(...data));
+
+// 2. make list of matched cities
+// 3. make html text code from the 'matched cities' list
+// 4. input the html text code
+// 5. make all these process to one fucntion
