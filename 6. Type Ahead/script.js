2ddaee36bca9295faf678c123774014cfb2154ff
bbang2 <power3869@gmail.com>
2023-01-26 00:02:32 +0900

    6.6. Type Ahead highlight the input value

--- 6. Type Ahead/script.js
+++ 6. Type Ahead/script.js
@@ -21,9 +21,12 @@ function findMatches(wordToMatch, cities) {
 function displayMatches() {
     const matchArray = findMatches(this.value, cities);
     const textCode = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
         return `
         <li>
-            <span class="name">${place.city}, ${place.state}</span>
+            <span class="name">${cityName}, ${stateName}</span>
             <span class="population">${place.population}</span>
         </li>
         `
@@ -37,4 +40,3 @@ const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
-// 5. make all these process to one fucntion
