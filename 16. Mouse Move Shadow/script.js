339178c84fb992fd1403b4d7cdd5cdf28629b291
bbang2 <power3869@gmail.com>
2023-02-14 19:39:43 +0900

    16.2. Mouse Move Shaodw - appoint x,y to center

--- 16. Mouse Move Shadow/script.js
+++ 16. Mouse Move Shadow/script.js
@@ -1,5 +1,7 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
+const centerX = text.offsetLeft + text.offsetWidth / 2;
+const centerY = text.offsetTop + text.offsetHeight / 2;
 
 function shadow(e) {
     let { offsetX: x, offsetY: y} = e;
@@ -7,7 +9,12 @@ function shadow(e) {
     if (this !== e.target) {
         x = x + e.target.offsetLeft;
         y = y + e.target.offsetTop;
+
+        x = x - centerX;
+        y = y - centerY;
     }
+
+
 
     text.style.textShadow = `
         ${x}px ${y}px 0 rgba(255,0,255,0.7),
