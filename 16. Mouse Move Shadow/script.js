8579439c829999d70627a52601b2cd7379862f04
bbang2 <power3869@gmail.com>
2023-02-14 20:27:36 +0900

    16.3. Mouse Move Shadow - express elegantly

--- 16. Mouse Move Shadow/script.js
+++ 16. Mouse Move Shadow/script.js
@@ -1,27 +1,24 @@
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
-const centerX = text.offsetLeft + text.offsetWidth / 2;
-const centerY = text.offsetTop + text.offsetHeight / 2;
 
 function shadow(e) {
     let { offsetX: x, offsetY: y} = e;
+    const walk = 100;
 
     if (this !== e.target) {
         x = x + e.target.offsetLeft;
         y = y + e.target.offsetTop;
-
-        x = x - centerX;
-        y = y - centerY;
     }
-
-
+    
+    const xWalk = ((x / hero.offsetWidth) - 0.5) * walk;
+    const yWalk = ((y / hero.offsetHeight) - 0.5) * walk;
 
     text.style.textShadow = `
-        ${x}px ${y}px 0 rgba(255,0,255,0.7),
-        ${x}px ${-y}px 0 rgba(0,255,255,0.7),
-        ${-x}px ${y}px 0 rgba(0,255,0,0.7),
-        ${-x}px ${-y}px 0 rgba(0,0,255,0.7)
+        ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+        ${xWalk}px ${-yWalk}px 0 rgba(0,255,255,0.7),
+        ${-xWalk}px ${yWalk}px 0 rgba(0,255,0,0.7),
+        ${-xWalk}px ${-yWalk}px 0 rgba(0,0,255,0.7)
     `;
 }
 
-window.addEventListener("mousemove", shadow);
+hero.addEventListener("mousemove", shadow);
