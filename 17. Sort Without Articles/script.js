6586dad01ac3956516757a0d3ec458f68c911101
bbang2 <power3869@gmail.com>
2023-02-15 11:27:17 +0900

    17.2 Sort Without Articles - Korean Language

--- 17. Sort Without Articles/script.js
+++ 17. Sort Without Articles/script.js
@@ -4,6 +4,8 @@ const titles = [
     "바람과 함께 사라지다", "올드보이", "킹덤", "오 나의 귀신님", "브레이킹 배드", "루시퍼", "왕좌의게임", "해리포터" 
 ];
 
+const sortedTitles = titles.sort((a, b) => a > b ? 1 : -1);
+
 const textCode = sortedTitles.map(title => {
     return `
         <li>${title}</li>
